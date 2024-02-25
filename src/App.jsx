0af3ff61877ecbe161e441318b2b26e4f4510e19
drankstar <137785834+drankstar@users.noreply.github.com>
2024-02-25 14:54:44 +0200

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,61 @@
-import { useState } from "react"
-import "./App.css"
+import { useEffect, useState } from "react"
 import ContactForm from "./components/ContactForm/ContactForm"
 import ContactList from "./components/ContactList/ContactList"
 import SearchBox from "./components/SearchBox/SearchBox"
 
+import styles from "./App.module.css"
+
 const toStandartRegister = (value) => value.toLocaleLowerCase()
 
-function App() {
-  const contacts = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]
+const initialContacts = () => {
+  const localStorageContacts = localStorage.getItem("myContacts")
+  return localStorageContacts !== null ? JSON.parse(localStorageContacts) : []
+}
 
+function App() {
   const [inputSearch, setInputSearch] = useState("")
+  const [contacts, setContacts] = useState(initialContacts)
+
+  useEffect(() => {
+    localStorage.setItem("myContacts", JSON.stringify(contacts))
+  }, [contacts])
 
   const onSearch = (evt) => {
     setInputSearch(evt.target.value)
   }
 
+  const addContacts = (contact) => {
+    setContacts((prev) => [...prev, contact])
+  }
+
   const filtredContacts = () => {
-    const filtred = contacts.filter((el) =>
+    if (!inputSearch) {
+      return contacts
+    }
+    return contacts.filter((el) =>
       toStandartRegister(el.name).includes(toStandartRegister(inputSearch))
     )
-    return filtred
+  }
+
+  const onDeleteContact = (id) => {
+    setContacts((prev) => prev.filter((el) => el.id !== id))
   }
 
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox inputSearch={inputSearch} onSearch={onSearch} />
-      <ContactList contacts={filtredContacts} />
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>Phonebook</h1>
+      <ContactForm addContacts={addContacts} />
+      {contacts.length ? (
+        <>
+          <SearchBox inputSearch={inputSearch} onSearch={onSearch} />
+          <ContactList
+            contacts={filtredContacts()}
+            onDeleteContact={onDeleteContact}
+          />
+        </>
+      ) : (
+        <p>Add your first contact</p>
+      )}
     </div>
   )
 }
